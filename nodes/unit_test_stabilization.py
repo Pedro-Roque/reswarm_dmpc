Updated solver params. Need to fix Hessian problem.

nodes/unit_test_stabilization.py
@@ -7,7 +7,7 @@ from reswarm_dmpc.controllers.mpc import MPC
 from reswarm_dmpc.simulation import EmbeddedSimEnvironment
 
 # Instantiante Model
-abee = Astrobee(h=0.05)
+abee = Astrobee(h=0.1)
 
 # Instantiate controller
 Q = np.diag([10, 10, 10, 100, 100, 100, 10, 10, 10, 100, 100, 100])
@@ -30,24 +30,30 @@ x_ref = np.array([.5, .5, .5, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0])
 ctl.set_reference(x_sp=x_ref)
 sim_env_full = EmbeddedSimEnvironment(model=abee,
                                       dynamics=abee.model,
+                                      ctl_class=ctl,
                                       controller=ctl.mpc_controller,
                                       time=20)
-sim_env_full.run([0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0])
+_, _, _, avg_t1 = sim_env_full.run([0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0])
 
 # Attitude setpoint
 x_ref = np.array([0, 0, 0, 0, 0, 0, 0.189, 0.038, 0.269, 0.944, 0, 0, 0])
 ctl.set_reference(x_sp=x_ref)
 sim_env_full = EmbeddedSimEnvironment(model=abee,
                                       dynamics=abee.model,
+                                      ctl_class=ctl,
                                       controller=ctl.mpc_controller,
                                       time=20)
-sim_env_full.run([0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0])
+_, _, _, avg_t2 = sim_env_full.run([0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0])
 
 # Pose setpoint
 x_ref = np.array([0.5, 0.5, 0.5, 0, 0, 0, 0.189, 0.038, 0.269, 0.944, 0, 0, 0])
 ctl.set_reference(x_sp=x_ref)
 sim_env_full = EmbeddedSimEnvironment(model=abee,
                                       dynamics=abee.model,
+                                      ctl_class=ctl,
                                       controller=ctl.mpc_controller,
                                       time=20)
-sim_env_full.run([0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0])
+_, _, _, avg_t3 = sim_env_full.run([0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0])
+
+# Print average times
+print"Average times:\nT1: ", avg_t1, "\nT2: ", avg_t2, "\nT3: ", avg_t3

src/reswarm_dmpc/simulation.py
@@ -9,7 +9,7 @@ import time
 
 
 class EmbeddedSimEnvironment(object):
-    def __init__(self, model, dynamics, controller, time=100.0):
+    def __init__(self, model, dynamics, ctl_class, controller, time=100.0):
         """
         Embedded simulation environment. Simulates the syste given
         dynamics and a control law, plots in matplotlib.
@@ -25,6 +25,7 @@ class EmbeddedSimEnvironment(object):
         """
         self.model = model
         self.dynamics = dynamics
+        self.ctl_class = ctl_class
         self.controller = controller
         self.total_sim_time = time  # seconds
         self.dt = self.model.dt
@@ -46,10 +47,10 @@ class EmbeddedSimEnvironment(object):
         y_vec = np.array([x0]).T
         ref_vec = np.array([x0]).T
         u_vec = np.array([[0, 0, 0, 0, 0, 0]]).T
-
+        slv_time = np.empty((1, 1))
         # Start figure
         if len(x0) == 13:
-            fig, (ax1, ax2, ax3, ax4) = plt.subplots(4)
+            fig, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(5)
         else:
             print("Check your state dimensions.")
             exit()
@@ -59,11 +60,10 @@ class EmbeddedSimEnvironment(object):
             x = np.array([y_vec[:, -1]]).T
 
             # Get control input and obtain next state
-            if self.using_trajectory_ref:
-                u, ref = self.controller(x, i*self.dt)
-                ref_vec = np.append(ref_vec, np.array([ref]).T, axis=1)
-            else:
-                u = self.controller(x)
+            u, ref = self.controller(x, i*self.dt)
+            ref_vec = np.append(ref_vec, np.array([ref]).T, axis=1)
+            slv_time = np.append(slv_time,
+                                 self.ctl_class.get_last_solve_time())
             x_next = self.dynamics(x, u)
 
             # Store data
@@ -79,54 +79,57 @@ class EmbeddedSimEnvironment(object):
                 l_wnd = 0
 
             # Plot type
-            if self.using_trajectory_ref:
-                ax1.clear()
-                ax1.set_title("Astrobee Testing")
-                ax1.plot(t[l_wnd:-1], y_vec[0, l_wnd:-1]-ref_vec[0, l_wnd:-1],
-                         t[l_wnd:-1], y_vec[1, l_wnd:-1]-ref_vec[1, l_wnd:-1],
-                         t[l_wnd:-1], y_vec[2, l_wnd:-1]-ref_vec[2, l_wnd:-1])
-                ax1.legend(["X", "Y", "Z"])
-                ax1.set_ylabel("Error [m]")
-                ax1.grid()
+            ax1.clear()
+            ax1.set_title("Astrobee Testing")
+            ax1.plot(t[l_wnd:-1], y_vec[0, l_wnd:-1]-ref_vec[0, l_wnd:-1],
+                     t[l_wnd:-1], y_vec[1, l_wnd:-1]-ref_vec[1, l_wnd:-1],
+                     t[l_wnd:-1], y_vec[2, l_wnd:-1]-ref_vec[2, l_wnd:-1])
+            ax1.legend(["e-x", "e-y", "e-z"])
+            ax1.set_ylabel("Error [m]")
+            ax1.grid()
 
-                ax2.clear()
-                ax2.set_title("Astrobee Testing")
-                ax2.plot(t[l_wnd:-1], y_vec[3, l_wnd:-1]-ref_vec[3, l_wnd:-1],
-                         t[l_wnd:-1], y_vec[4, l_wnd:-1]-ref_vec[4, l_wnd:-1],
-                         t[l_wnd:-1], y_vec[5, l_wnd:-1]-ref_vec[5, l_wnd:-1])
-                ax2.legend(["X", "Y", "Z"])
-                ax2.set_ylabel("Error [m]")
-                ax2.grid()
+            ax2.clear()
+            ax2.plot(t[l_wnd:-1], y_vec[3, l_wnd:-1]-ref_vec[3, l_wnd:-1],
+                     t[l_wnd:-1], y_vec[4, l_wnd:-1]-ref_vec[4, l_wnd:-1],
+                     t[l_wnd:-1], y_vec[5, l_wnd:-1]-ref_vec[5, l_wnd:-1])
+            ax2.legend(["e-vx", "e-vy", "e-vz"])
+            ax2.set_ylabel("Error [m/s]")
+            ax2.grid()
 
-                ax3.clear()
-                ax3.plot(t[l_wnd:-1], u_vec[0, l_wnd:-1],
-                         t[l_wnd:-1], u_vec[1, l_wnd:-1],
-                         t[l_wnd:-1], u_vec[2, l_wnd:-1])
-                ax3.set_xlabel("Time [s]")
-                ax3.set_ylabel("Control [u]")
-                ax3.grid()
-            else:
-                ax1.clear()
-                ax1.set_title("Astrobee Setpoint Test")
-                ax1.plot(t[l_wnd:-1], y_vec[0, l_wnd:-1],
-                         t[l_wnd:-1], y_vec[1, l_wnd:-1],
-                         t[l_wnd:-1], y_vec[2, l_wnd:-1])
-                ax1.legend(["X", "Y", "Z"])
-                ax1.set_ylabel("Position [m]")
-                ax1.grid()
+            # Plot attitude
+            ax3.clear()
+            qerr = np.array([[0]])
+            q = y_vec[6:10, l_wnd:-1]
+            q_des = ref_vec[6:10, l_wnd:-1]
+            for j in range(len(q_des.T)):
+                qerr = np.concatenate((qerr, np.array([[1 - np.dot(q[:, j].T, q_des[:, j])**2]])), axis=1)
+            qerr = qerr[:, 1:]  # Remove quaternion error initialization
+            ax3.plot(t[l_wnd:-1], qerr.T)
+            ax3.set_xlabel("Time [s]")
+            ax3.set_ylabel("Attitude Error [norm-u]")
+            ax3.grid()
 
-                ax2.clear()
-                ax2.plot(t[l_wnd:-1], u_vec[0, l_wnd:-1],
-                         t[l_wnd:-1], u_vec[1, l_wnd:-1],
-                         t[l_wnd:-1], u_vec[2, l_wnd:-1])
-                ax2.set_xlabel("Time [s]")
-                ax2.set_ylabel("Control [u]")
-                ax2.grid()
+            # Plot controls
+            ax4.clear()
+            ax4.plot(t[l_wnd:-1], u_vec[0, l_wnd:-1],
+                     t[l_wnd:-1], u_vec[1, l_wnd:-1],
+                     t[l_wnd:-1], u_vec[2, l_wnd:-1])
+            ax4.set_xlabel("Time [s]")
+            ax4.set_ylabel("Control F [N]")
+            ax4.grid()
 
-            plt.pause(0.01)
+            ax5.clear()
+            ax5.plot(t[l_wnd:-1], u_vec[3, l_wnd:-1],
+                     t[l_wnd:-1], u_vec[4, l_wnd:-1],
+                     t[l_wnd:-1], u_vec[5, l_wnd:-1])
+            ax5.set_xlabel("Time [s]")
+            ax5.set_ylabel("Control T [Nm]")
+            ax5.grid()
 
         plt.show()
-        return t, y_vec, u_vec
+        plt.pause(0.01)
+
+        return t, y_vec, u_vec, np.average(slv_time)
 
     def set_window(self, window):
         """