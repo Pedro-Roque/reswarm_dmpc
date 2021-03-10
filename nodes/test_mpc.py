4a92ae9b9a7c535f8dc4db12cd0c65ede0c8461c
Pedro-Roque <padr@kth.se>
2021-03-10 16:54:56 +0100

    Added extra time for pose test.

--- nodes/test_mpc.py
+++ nodes/test_mpc.py
@@ -109,7 +109,7 @@ class ControllerStabilizationTest(unittest.TestCase):
                                               dynamics=abee.model,
                                               ctl_class=ctl,
                                               controller=ctl.mpc_controller)
-        _, y, _, avg_t1 = sim_env_full.run([0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0])
+        _, y, _, _ = sim_env_full.run([0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0])
 
         self.assertTrue(np.array_equal(np.around(y[:, -1], 2), x_ref))
 
@@ -146,9 +146,45 @@ class ControllerStabilizationTest(unittest.TestCase):
                                               ctl_class=ctl,
                                               controller=ctl.mpc_controller, 
                                               time=20.0)
-        _, y, _, avg_t1 = sim_env_full.run([0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0])
+        _, y, _, _ = sim_env_full.run([0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0])
 
         self.assertTrue(np.array_equal(np.around(y[:, -1], 3), x_ref))
+
+    def test_pose_setpoint(self):
+        """
+        Test pose setpoints.
+        Evaluates if the controller can converge, in the default time,
+        to the desired pose setpoint.
+        """
+        # Instantiante Model
+        abee = Astrobee(h=0.1, iface='casadi')
+
+        # Instantiate controller
+        Q = np.diag([10, 10, 10, 100, 100, 100, 10, 10, 10, 100, 100, 100])
+        R = np.diag([1, 1, 1, 0.5, 0.5, 0.5])*10
+        P = Q*100
+
+        ctl = MPC(model=abee,
+                  dynamics=abee.model,
+                  horizon=.5,
+                  Q=Q, R=R, P=P,
+                  ulb=[-1, -1, -1, -0.1, -0.1, -0.1],
+                  uub=[1, 1, 1, 0.1, 0.1, 0.1],
+                  xlb=[-1, -1, -1, -0.1, -0.1, -0.1,
+                       -1, -1, -1, -1, -0.1, -0.1, -0.1],
+                  xub=[1, 1, 1, 0.1, 0.1, 0.1,
+                       1, 1, 1, 1, 0.1, 0.1, 0.1])
+
+        # Position setpoint
+        x_ref = np.array([0.5, 0.5, 0.5, 0, 0, 0, 0.189, 0.038, 0.269, 0.944, 0, 0, 0])
+        ctl.set_reference(x_sp=x_ref)
+        sim_env_full = EmbeddedSimEnvironment(model=abee,
+                                              dynamics=abee.model,
+                                              ctl_class=ctl,
+                                              controller=ctl.mpc_controller)
+        _, y, _, _ = sim_env_full.run([0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0])
+
+        self.assertTrue(np.array_equal(np.around(y[:, -1], 2), x_ref))
 
 
 if __name__ == '__main__':
