b53af43f7401470c37b7b259b4722e5e0f334647
Pedro-Roque <padr@kth.se>
2021-03-10 16:29:20 +0100

    Added test for stabilization.

--- nodes/test_mpc.py
+++ nodes/test_mpc.py
@@ -4,6 +4,7 @@ import casadi as ca
 
 from reswarm_dmpc.models.astrobee import Astrobee
 from reswarm_dmpc.controllers.mpc import MPC
+from reswarm_dmpc.simulation import EmbeddedSimEnvironment
 
 
 class ControllerStabilizationTest(unittest.TestCase):
@@ -81,7 +82,36 @@ class ControllerStabilizationTest(unittest.TestCase):
         Check steady-state.
         """
 
-        self.assertTrue(True)
+        # Instantiante Model
+        abee = Astrobee(h=0.1, iface='casadi')
+
+        # Instantiate controller
+        Q = np.diag([10, 10, 10, 100, 100, 100, 10, 10, 10, 100, 100, 100])
+        R = np.diag([1, 1, 1, 0.5, 0.5, 0.5])*10
+        P = Q*100
+
+        ctl = MPC(model=abee,
+                  dynamics=abee.model,
+                  horizon=.5,
+                  Q=Q, R=R, P=P,
+                  ulb=[-1, -1, -1, -0.1, -0.1, -0.1],
+                  uub=[1, 1, 1, 0.1, 0.1, 0.1],
+                  xlb=[-1, -1, -1, -0.1, -0.1, -0.1,
+                       -1, -1, -1, -1, -0.1, -0.1, -0.1],
+                  xub=[1, 1, 1, 0.1, 0.1, 0.1,
+                       1, 1, 1, 1, 0.1, 0.1, 0.1])
+
+        # Position setpoint
+        x_ref = np.array([.5, .5, .5, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0])
+        ctl.set_reference(x_sp=x_ref)
+        sim_env_full = EmbeddedSimEnvironment(model=abee,
+                                              dynamics=abee.model,
+                                              ctl_class=ctl,
+                                              controller=ctl.mpc_controller,
+                                              time=100)
+        _, y, _, avg_t1 = sim_env_full.run([0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0])
+
+        self.assertTrue(np.array_equal(np.around(y[:, -1], 3), x_ref))
 
 
 if __name__ == '__main__':
