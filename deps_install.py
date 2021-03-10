d925e2dcb4541512b9a4d5b1430b845edd7ff9af
Pedro-Roque <padr@kth.se>
2021-03-10 18:16:41 +0100

    Updated dependencies install script.

--- deps_install.py
+++ deps_install.py
@@ -10,39 +10,4 @@ if __name__ == "__main__":
     install("numpy")
     install("control")
     install("scipy")
-
-    try:
-        import matplotlib
-    except :
-        print("ERROR: Matplotlib package not installed. Please install it manually for your environment")
-        exit()
-    
-    try:
-        import casadi
-    except :
-        print("ERROR: CasADi package not installed. Please install it manually for your environment")
-        exit()
-
-    try:
-        import numpy
-    except :
-        print("ERROR: Numpy package not installed. Please install it manually for your environment")
-        exit()
-
-    try:
-        import control
-    except :
-        print("ERROR: Control package not installed. Please install it manually for your environment")
-        exit()
-
-    try:
-        import scipy
-    except :
-        print("ERROR: Scipy package not installed. Please install it manually for your environment")
-        exit()
-
-    try:
-        import filterpy
-    except :
-        print("ERROR: filterpy package not installed. Please install it manually for your environment")
-        exit()
+    install("unittest-parallel")
