f919039a875a7459f4f2d64c45a66f8d3139875c
Pedro-Roque <padr@kth.se>
2021-07-06 20:27:13 +0200

    fix: removed dynamic reconfigure, cross compiles fine

---
+++ src/reswarm_dmpc/controllers/formation/acado/qpoases/SRC/acado_dummy_file.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+#include "acado_common.h"
+#include "acado_auxiliary_functions.h"
+
+/** Instance of the user data structure. */
+ACADOvariables acadoVariables;
+/** Instance of the private workspace structure. */
+ACADOworkspace acadoWorkspace;
