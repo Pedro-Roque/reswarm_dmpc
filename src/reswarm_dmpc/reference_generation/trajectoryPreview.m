0b73d5208f18165fdf8b63e8524dfaaaf6fda0a2
Pedro-Roque <padr@kth.se>
2021-08-05 16:43:06 +0200

    fix: tests are now within recommended ISS envelope

--- src/reswarm_dmpc/reference_generation/trajectoryPreview.m
+++ src/reswarm_dmpc/reference_generation/trajectoryPreview.m
@@ -1,18 +1,18 @@
-init = [0.0, 0.0, 0.0];
-Avx = 0.06;
+init = [10.75, -8.5, 4.3];
+Avx = 0.03;
 Avy = -0.005;
-Avz = 0.06;
+Avz = 0.03;
 f = 0.035;
-dt = 0.5;
+dt = 0.1;
 
 x = init(1);
 y = init(2);
 z = init(3);
 
-t = 0:dt:120;
-vx = A*cos(2*pi*f*t);
-vy = -0.005*ones(length(t));
-vz = A*sin(2*pi*f*t);
+t = 0:dt:60;
+vx = Avx*cos(2*pi*f*t);
+vy = Avy*ones(length(t));
+vz = Avz*sin(2*pi*f*t);
 
 for i=1:length(t)
    x = [x, x(end)+vx(i)*dt];
@@ -20,4 +20,11 @@ for i=1:length(t)
    z = [z, z(end)+vz(i)*dt];
 end
 
+% Position Profile
 plot3(x,y,z)
+
+% Velocity Profile
+figure()
+plot(t, vx); hold on
+plot(t, vy); hold on
+plot(t, vz); hold off
