f35ff31e88e3f8004da009705a3e0e4be95e6dda
Pedro-Roque <padr@kth.se>
2021-11-12 13:25:35 +0100

    add: added ground param generators

---
+++ nodes/param_gen_fc_leader_gnd.py
@@ -0,0 +1,79 @@
+
+# Generator for formation tests - subleader
+import json
+
+traj_profiles = [1, 2, 3]
+
+# Test 1 components
+unit_test_time = [60, 60, 60]
+expiration_time = [120, 60, 60]
+bearings = {'f1': [-0.8, 0.0, 0.0]}
+traj_start = [0.5, -0.5, -0.76]
+qd = [0, 0, -0.707, 0.707]
+traj_velocity_1 = {'t1': [0.0, 0.01, 0.0], 't2': [0.02, 0.01, 0.0]}
+traj_velocity_2 = {'t2': [0.02, 0.01, 0.0]}
+traj_velocity_3 = {'t1': [0.0, 0.01, 0.0]}
+
+wr_l = [5, 20, 100]
+wp_l = [10, 100]
+wv_l = [10, 100]
+wa_l = [10, 100]
+ww_l = [10, 100]
+
+wf_l = [5, 25, 50]
+wt_l = [5, 25, 50]
+
+for profile in traj_profiles:
+    print("Test: ", profile)
+    setting_counter = 0
+    for wr in wr_l:
+        for wp in wp_l:
+            for wv in wv_l:
+                for wa in wa_l:
+                    for ww in ww_l:
+                        for wf in wf_l:
+                            for wt in wt_l:
+
+                                if wp == wv or wa == ww:
+                                    continue
+
+                                utt = unit_test_time[profile - 1]
+                                exp_t = expiration_time[profile - 1]
+
+                                wr_v = [wr] * 3
+                                wp_v = [wp] * 3
+                                wv_v = [wv] * 3
+                                wa_v = [wa] * 3
+                                ww_v = [ww] * 3
+
+                                wf_v = [wf] * 3
+                                wt_v = [wt] * 3
+
+                                W_l = wr_v + wp_v + wv_v + wa_v + ww_v + wf_v + wt_v
+                                W_N_l = wr_v + wp_v + wv_v + wa_v + ww_v
+
+                                yaml_write = {'test_time': utt}
+                                yaml_write['expiration_time'] = exp_t
+                                yaml_write['W'] = W_l
+                                yaml_write['WN'] = W_N_l
+                                yaml_write['WN_gain'] = 200
+                                yaml_write['WN_gain'] = 200
+                                yaml_write['bearings'] = bearings
+                                yaml_write['qd'] = qd
+                                yaml_write['traj_start'] = traj_start
+
+                                qd = [0, 0, -0.707, 0.707]
+                                if profile == 1:
+                                    yaml_write['traj_velocity'] = traj_velocity_1
+                                elif profile == 2:
+                                    yaml_write['traj_velocity'] = traj_velocity_2
+                                elif profile == 3:
+                                    yaml_write['traj_velocity'] = traj_velocity_3
+
+                                file_name = 'leader_iface_gnd_3' + str(profile) + "{:03d}".format(setting_counter)
+
+                                with open('/home/roque/reswarm_ws/src/reswarm_dmpc/config/honey/' + file_name + '.yaml', 'w') as file:
+                                    json.dump(yaml_write, file)
+
+                                setting_counter = setting_counter + 1
+                                print(file_name)
---
+++ nodes/param_gen_ut_secondary_gnd.py
@@ -0,0 +1,82 @@
+# Generator for unit tests - secondary
+import json
+
+test_list = [1, 2]
+
+# Test 1 components
+unit_test_time = [45, 45]
+expiration_time = [310, 310]
+targets_t1 = {'t': {
+    't1': [-0.5, -0.5, -0.67, 0, 0, 0, 0.707, -0.707, 0.0, 0.0, 0, 0, 0],
+    't2': [-0.5, -0.1, -0.67, 0, 0, 0, 0.707, -0.707, 0.0, 0.0, 0, 0, 0],
+    't3': [-0.5, -0.5, -0.67, 0, 0, 0, 0.707, -0.707, 0.0, 0.0, 0, 0, 0],
+    't4': [-0.1, -0.5, -0.67, 0, 0, 0, 0.707, -0.707, 0.0, 0.0, 0, 0, 0],
+    't5': [-0.5, -0.5, -0.67, 0, 0, 0, 0.707, -0.707, 0.0, 0.0, 0, 0, 0]}
+              }
+
+targets_t2 = {'q': {
+    't1': [-0.5, -0.5, -0.67, 0, 0, 0, 0.707, -0.707, 0.0, 0.0, 0, 0, 0],
+    't2': [-0.5, -0.5, -0.67, 0, 0, 0, 0.866, -0.500, 0.0, 0.0, 0, 0, 0],
+    't3': [-0.5, -0.5, -0.67, 0, 0, 0, 0.707, -0.707, 0.0, 0.0, 0, 0, 0]}
+              }
+
+wp_l = [10, 100]
+wv_l = [10, 100]
+wa_l = [10, 100]
+ww_l = [10, 100]
+
+wf_l = [5, 25, 50]
+wt_l = [5, 25, 50]
+
+for test in test_list:
+    print("Test: ", test)
+    setting_counter = 50
+    for wp in wp_l:
+        for wv in wv_l:
+            for wa in wa_l:
+                for ww in ww_l:
+                    for wf in wf_l:
+                        for wt in wt_l:
+
+                            if wp == wv or wa == ww:
+                                continue
+
+                            utt = unit_test_time[test - 1]
+                            exp_t = expiration_time[test - 1]
+                            if test == 1:
+                                test_type = 'translation'
+                                target = targets_t1
+                            else:
+                                test_type = 'attitude'
+                                target = targets_t2
+
+                            wp_v = [wp] * 3
+                            wv_v = [wv] * 3
+                            wa_v = [wa] * 3
+                            ww_v = [ww] * 3
+
+                            wf_v = [wf] * 3
+                            wt_v = [wt] * 3
+
+                            W_l = wp_v + wv_v + wa_v + ww_v + wf_v + wt_v
+                            W_N_l = wp_v + wv_v + wa_v + ww_v
+
+                            yaml_write = {'test_num': test}
+                            yaml_write['unit_test_time'] = utt
+                            yaml_write['expiration_time'] = exp_t
+                            yaml_write['W'] = W_l
+                            yaml_write['WN'] = W_N_l
+                            yaml_write['WN_gain'] = 200
+                            yaml_write['WN_gain'] = 200
+                            yaml_write['targets'] = target
+
+                            file_name = 'test_' + test_type + '_gnd_' + str(test) + "{:02d}".format(setting_counter)
+
+                            with open('/home/roque/reswarm_ws/src/reswarm_dmpc/config/bumble/' + file_name + '.yaml', 'w') as file:
+                                json.dump(yaml_write, file)
+
+                            setting_counter = setting_counter + 1
+                            print("Settings counter: ", setting_counter)
+                            if setting_counter > 99:
+                                print("Oooops")
+                                break
